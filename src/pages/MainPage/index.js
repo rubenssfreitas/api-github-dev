d62d91481db0261ee7499947fd557303209cb03e
rubenssfreitas <rubens.suporte@gmail.com>
2023-03-02 17:47:08 -0300

    concluido

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {MdSearch} from 'react-icons/md';
 
-import { Container, Logo } from './styles';
+import { Container, Logo, Title, Form, Input, Button } from './styles';
 
 import githublogo from '../../assets/images/github-logo.svg';
 
 const MainPage = () => (
   <Container>
     <Logo src={githublogo} alt="API GitHub" />
+    <Title>API GitHub</Title>
+    <Form>
+
+      <Input placeholder="Usuario" />
+      <Button>
+        <MdSearch size={42} />
+      </Button>
+    </Form>
   </Container>
 );
 
--- src/pages/MainPage/styles.js
+++ src/pages/MainPage/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content; center;
+  justify-content: center;
   text-align: center;
   min-height: 100vh;
   padding: 0 2rem;
@@ -12,4 +12,48 @@ export const Container = styled.div`
 
 export const Logo = styled.img`
   width: 130px;
+`;
+
+export const Title = styled.h1`
+  font-size: 2.2rem;
+  color: ${(props) => props.theme.colors.text};
+`;
+
+export const Form = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 583px;
+`;
+
+export const Input = styled.input`
+  background: rgba(0, 0, 0, 0.8);
+  width: 100%;
+  heigth: 60px;
+  line-height: 64px;
+  border: none;
+  font-size: 24px;
+  margin-rigth: 24px;
+  padding: 0 24px;
+
+  &::placeholder {
+    color:${(props) => props.theme.colors.metalDark};
+  }
+`;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: transparent;
+  width: 60px;
+  heigth: 64px;
+  border: 3px solid ${(props) => props.theme.colors.text};
+  transition: background 0.3s;
+
+  &::hover {
+    background: ${(props) => props.theme.colors.container};
+  }
 `;
