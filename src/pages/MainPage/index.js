526f363059113cf2b0a86086f510e1a073bcf144
rubenssfreitas <rubens.suporte@gmail.com>
2023-03-02 15:35:31 -0300

    ajustes innit

---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const MainPage = () => (
+  <Container>
+    <img
+      src="https://devsamurai-materials.s3.amazonaws.com/templates/dev-samurai-white.png"
+      height="256"
+      alt="Dev Samurai"
+    />
+    <h1>Fala Samurai!</h1>
+    <p>Esse é o template básico da Dev Samurai para React.</p>
+  </Container>
+);
+
+export default MainPage;
